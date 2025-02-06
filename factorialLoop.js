ccdd99787281037111b2f5c352e0588ef8f6be87
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-06 11:25:00 +0530

    adding factorial loop

---
+++ factorialLoop.js
@@ -0,0 +1,6 @@
+let n = 5, fact = 1;
+for(let i = 1; i <= n; i++) {
+  fact *= i;
+}
+console.log("Factorial:", fact);
+// This code calculates the factorial of a number n using a loop and prints the result to the console.
