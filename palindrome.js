6fdb859d9fa3f23ce0cda2d14cdb6ad8445c37e1
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-08 11:25:00 +0530

    adding check palindrome number

---
+++ palindrome.js
@@ -0,0 +1,4 @@
+let str = "madam";
+let isPal = str === str.split("").reverse().join("");
+console.log("Palindrome?", isPal);
+// This code checks if a string is a palindrome by comparing it to its reverse and prints the result to the console.
