73d4e225278326fd4b1d2f2f0601464bbb867953
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-09 11:25:00 +0530

    adding calculator

---
+++ calculator.js
@@ -0,0 +1,9 @@
+let a = 12, b = 4, op = "*";
+switch(op) {
+  case "+": console.log(a + b); break;
+  case "-": console.log(a - b); break;
+  case "*": console.log(a * b); break;
+  case "/": console.log(a / b); break;
+  default: console.log("Invalid operator");
+}
+// This code performs a basic arithmetic operation based on the operator provided and prints the result to the console.
