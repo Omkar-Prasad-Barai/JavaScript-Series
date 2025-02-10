f6defaf1d89c5fe97d93c86284159fe75b333722
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-10 11:25:00 +0530

    adding sum of array

---
+++ sumOfArraay.js
@@ -0,0 +1,4 @@
+let arr = [3, 6, 9];
+let sum = arr.reduce((acc, val) => acc + val, 0);
+console.log("Sum:", sum);
+// This code calculates the sum of all elements in an array using the reduce method and prints the result to the console.
