2fc687ff67e97376434e2e9a4ee7eddeeddf6d83
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-13 11:25:00 +0530

    adding multiplication

---
+++ multiplication.js
@@ -0,0 +1,5 @@
+let n = 5;
+for(let i = 1; i <= 10; i++) {
+  console.log(`${n} x ${i} = ${n * i}`);
+}
+// This code generates the multiplication table for the number n (5 in this case) from 1 to 10 and prints each line in the format "n x i = result".
