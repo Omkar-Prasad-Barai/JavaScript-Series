096b4a3db3bdac610fc197a5ccea2e5eb92d0fb2
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-15 11:25:00 +0530

    adding celcious to faranhite

---
+++ celsiousToFara.js
@@ -0,0 +1,4 @@
+let c = 30;
+let f = (c * 9/5) + 32;
+console.log(`${c}°C = ${f}°F`);
+// This code converts a temperature from Celsius to Fahrenheit and prints the result in the format "c°C = f°F".
