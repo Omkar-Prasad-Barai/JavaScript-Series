a6ae85a7151b2499f4de99a7c6cae5557d155a73
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-19 11:25:00 +0530

    adding random number

---
+++ RandomNum.js
@@ -0,0 +1,4 @@
+let min = 10, max = 20;
+let rand = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log("Random:", rand);
+// This code generates a random number between min (10) and max (20) inclusive, and prints the result to the console.
