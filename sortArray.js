49f1599ff27d909a00a72df205529e44b3d8e33f
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-20 11:25:00 +0530

    adding sort array

---
+++ sortArray.js
@@ -0,0 +1,4 @@
+let names = ["Omkar", "Barai", "Samriddhi"];
+names.sort();
+console.log(names);
+// This code sorts an array of names in alphabetical order and prints the sorted array to the console.
