0dd656419204b1d2af3d0eef162103a3bf23db70
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-02-28 11:25:00 +0530

    adding find second largest number

---
+++ findSecondLargeNum.js
@@ -0,0 +1,4 @@
+let arr = [4, 7, 2, 9, 5];
+arr.sort((a, b) => b - a);
+console.log("Second largest:", arr[1]);
+// This code sorts an array in descending order and prints the second largest number to the console.
