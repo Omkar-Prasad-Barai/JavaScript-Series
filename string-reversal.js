176b8e1f8af5c0a5d9717540b92487c1243cb478
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-03-08 09:35:00 +0530

    adding string reversal

---
+++ string-reversal.js
@@ -0,0 +1,5 @@
+// 36-string-reversal.js
+const str = "javascript";
+const reversed = str.split("").reverse().join("");
+console.log(reversed); // tpircsavaj
+// This code reverses a string by splitting it into an array of characters, reversing the array, and then joining it back into a string. The reversed string is printed to the console.
