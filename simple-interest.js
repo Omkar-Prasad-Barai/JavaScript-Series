ff2345f6b5e39334c3b0a2fa42c715fbaad430de
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-03-16 09:35:00 +0530

    adding simple intrest

---
+++ simple-interest.js
@@ -0,0 +1,7 @@
+// 45-simple-interest.js
+const principal = 1000;
+const rate = 5;
+const time = 2;
+const simpleInterest = (principal * rate * time) / 100;
+console.log("Simple Interest:", simpleInterest); // 100
+// This code calculates the simple interest using the formula: (Principal * Rate * Time) / 100.
