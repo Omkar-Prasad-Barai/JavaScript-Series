b5bd724a180e97dd035a6b68453384848c1ef276
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-03-20 09:35:00 +0530

    adding swap variables

---
+++ swap-variables.js
@@ -0,0 +1,5 @@
+// 47-swap-variables.js
+let a = 5, b = 10;
+[a, b] = [b, a];
+console.log("a:", a, "b:", b); // a: 10 b: 5
+// This code swaps the values of two variables a and b using array destructuring and prints the swapped values to the console.
