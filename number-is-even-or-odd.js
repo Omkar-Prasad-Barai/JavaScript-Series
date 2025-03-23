c6e6876df3d8f8a2da479bde486e17f306c4380b
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2025-03-23 09:35:00 +0530

    adding number is even or odd

---
+++ number-is-even-or-odd.js
@@ -0,0 +1,7 @@
+// 49-number-is-even-or-odd.js
+function checkEvenOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
+console.log(checkEvenOdd(7)); // Odd
+console.log(checkEvenOdd(10)); // Even
+console.log(checkEvenOdd(0)); // Even
